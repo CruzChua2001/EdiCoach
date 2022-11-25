06854645413e19f913645388296338525705c4ac
CruzChua2001 <89196728+CruzChua2001@users.noreply.github.com>
2022-11-25 18:25:29 +0800

    Changes for coaches and bootstrap

--- src/coach/components/App.js
+++ src/coach/components/App.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import styled from "styled-components";
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function App() {
-	return <h1>Welcome to My Project 4</h1>;
+import Appointment from "./Appointment";
+import Navitem from "./Navitem";
+
+const TopNav = styled.div`
+	z-index: 999;
+	display: relative;
+`
+
+const LeftNav = styled.div`
+	float: left;
+`
+
+const RightNav = styled.div`
+	text-align: right;
+`
+
+const App = () => {
+	return (
+		<Router>
+			<TopNav>
+				<LeftNav></LeftNav>
+				<RightNav>
+					<Navitem />
+				</RightNav>
+			</TopNav>
+
+			<Button variant={'primary'}>test</Button>
+
+			<Routes>
+				<Route path="/coach/" element={<Appointment />}/>
+			</Routes>
+		</Router>
+	)
 }
+
+export default App
--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const PORT = 3000;
+const PORT = 3001;
 const STATIC_ASSETS_PATH = path.resolve(`${__dirname}/../../static`);
 
 const app = express();
@@ -9,7 +9,11 @@ const app = express();
 // Serve front end assets which have been built by webpack
 app.use("/static", express.static(STATIC_ASSETS_PATH));
 
-app.get("/*", (request, response) => {
+app.get("/", (request, response) => {
+	response.redirect("/guest/");
+});
+
+app.get("/guest/*", (request, response) => {
 	response.sendFile(path.join(__dirname + '/view/guest/index.html'));
 });
 
