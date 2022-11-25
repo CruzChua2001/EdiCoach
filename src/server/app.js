426fed2fcdc8a578d69080949d7ec43d83c72286
CruzChua2001 <89196728+CruzChua2001@users.noreply.github.com>
2022-11-25 14:11:00 +0800

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -9,7 +9,11 @@ const app = express();
 // Serve front end assets which have been built by webpack
 app.use("/static", express.static(STATIC_ASSETS_PATH));
 
-app.get("/*", (request, response) => {
+app.get("/", (request, response) => {
+	response.redirect("/guest/")
+})
+
+app.get("/guest/*", (request, response) => {
 	response.sendFile(path.join(__dirname + '/view/guest/index.html'));
 });
 
