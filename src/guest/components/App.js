f2e35387ea6d8b5edd31417e85672d92912bf8ac
Xuan Wei <joshualim2122@gmail.com>
2022-11-28 01:22:33 +0800

    Personalization Page

--- src/guest/components/App.js
+++ src/guest/components/App.js
@@ -3,21 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 
-const TopNav = styled.div`
-	z-index: 999;
-	display: relative;
-	margin-inline: 3%;
-	margin-block: 2%
-`
-
-const LeftNav = styled.div`
-	float: left;
-`
-    
-const RightNav = styled.div`
-	text-align: right;
-`	
-
 import Home from "./Home";
 import Careercoaching from "./Careercoaching";
 import Contact from "./Contact";
@@ -25,6 +10,7 @@ import Login from "./Login";
 import Signup from "./Signup";
 import OneonOnecoaching from "./OneonOnecoaching";
 import NavBar from "./Navbar";
+import Personlization from "./Personalization";
 
 export default function App() {
 	return (
@@ -38,6 +24,7 @@ export default function App() {
 				<Route path="/guest/contact" element={<Contact />} />
 				<Route path="/guest/login" element={<Login />} />
 				<Route path="/guest/sign-up" element={<Signup />} />
+				<Route path="/guest/personalization" element={<Personlization />} />
 			</Routes>
 
 		</Router>
--- src/guest/components/Signup.js
+++ src/guest/components/Signup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 const Signup = () => {
 
