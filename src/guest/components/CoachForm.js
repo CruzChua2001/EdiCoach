6624d393111a5a8f41960a69f13781c8bdc2c1f1
CruzChua2001 <89196728+CruzChua2001@users.noreply.github.com>
2022-12-07 09:50:10 +0800

    Update CoachForm.js

--- src/guest/components/CoachForm.js
+++ src/guest/components/CoachForm.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Form, Button } from 'react-bootstrap';
 import bcrypt from 'bcryptjs';
 import { v4 as uuidv4 } from 'uuid';
-import 'C:/Users/ashle/Documents/Application Security Project/EdiCoach/src/guest/css/gueststyle.css';
 import { SortNumericDown } from "react-bootstrap-icons";
 import { Message } from "@mui/icons-material";
 
