5cf8d88ce3746f68828b6b8691762c053698db3b
Edison-choo <chooedison@yahoo.com>
2023-02-09 18:58:23 +0800

    login

--- src/guest/components/Login.js
+++ src/guest/components/Login.js
@@ -1,13 +1,17 @@
 import React, { useContext } from "react";
 import {Form, Button} from 'react-bootstrap';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { OAuth2Client } from 'google-auth-library';
 
 import bcrypt from 'bcryptjs';
-import { useCookies } from 'react-cookie';
 
 import config from '../../../config';
 import { AccountContext } from "../../Account";
 
 import { CognitoUserPool, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
+
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
 const poolData = config.poolData;
 
@@ -21,28 +25,29 @@ const Login = () => {
         height: '80vh',
       };
 
-      const [cookies, setCookie, removeCookie] = useCookies(['name']);
-
       const Login = () => {
           let email = document.getElementById("email").value;
           let password = document.getElementById("password").value;
   
-          fetch(config.TEST_API+"/getusertype/"+email, {
+          fetch(config.USER_MANAGEMENT_API+"/getusertype/"+email, {
           method: 'GET',
           headers: { 'Content-Type': 'application/json' }})
           .then((msg) => {
               msg.json()
-              .then(type => {
-                  if (type == "Error") {
+              .then(body => {
+                if (body.usertype == "Error") {
                     document.getElementById("errorMessage").innerText = "Email or password is incorrect";
-                      
-  
-                  } else if (type !== "Client") {
+                } else if (body.usertype !== "Client") {
                     document.getElementById("errorMessage").innerText = "Login in coach login page instead";
-                }else {
-  
+                } else if (body.registration == "google") {
+                    document.getElementById("errorMessage").innerText = "Email is registed with google authentication";
+                } else {
+
                     authenticate(email, password)
                     .then((data) => {
+                        // console.log(data)
+                        cookies.set('accessToken', data.accessToken.jwtToken, { path: '/' });
+                        cookies.set('userType', body.usertype, { path: '/' });
                         location.href = "/client/";
                     })
                     .catch((err) => {
@@ -59,8 +64,58 @@ const Login = () => {
           
       }
   
-      const Logout = () => {
-          removeCookie("sessionId");
+      const onSuccess = async (res) => {
+        console.log(res);
+
+        let googlePayload
+        const oauthClient = new OAuth2Client(config.NEXT_PUBLIC_GOOGLE_CLIENT_ID)
+        const ticket = await oauthClient.verifyIdToken({
+        idToken: res.credential,
+        audience: config.NEXT_PUBLIC_GOOGLE_CLIENT_ID
+        })
+        googlePayload = ticket.getPayload()
+        console.log(ticket)
+        console.log(googlePayload)
+
+        if (googlePayload.iss == "https://accounts.google.com" && googlePayload.aud == config.NEXT_PUBLIC_GOOGLE_CLIENT_ID) {
+            fetch(config.USER_MANAGEMENT_API+"/getusertype/"+googlePayload.email, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }})
+                .then((msg) => {
+                    msg.json()
+                    .then(body => {
+                        if (body == "Error") {
+                            document.getElementById("errorMessage").innerText = "Email or password is incorrect";
+                        } else if (body.usertype !== "Client") {
+                            document.getElementById("errorMessage").innerText = "Login in coach login page instead";
+                        } else if (body.registration == "website") {
+                            document.getElementById("errorMessage").innerText = "Email is registed with website authentication";
+                        } else {
+        
+                          authenticate(googlePayload.email, googlePayload.sub)
+                          .then((data) => {
+                              // console.log(data)
+                              cookies.set('accessToken', data.accessToken.jwtToken, { path: '/' });
+                              cookies.set('userType', body.usertype, { path: '/' });
+                              location.href = "/client/";
+                          })
+                          .catch((err) => {
+                              document.getElementById("errorMessage").innerHTML = err;
+                              document.getElementById("errorMessage").display = "block";
+                          })
+        
+                        }
+        
+                    });
+                    
+                    
+                }).catch(err => console.log(err))
+        }
+
+      }
+
+      const onFailure = (res) => {
+        console.log(res);
       }
     
     return ( <>
@@ -82,10 +137,19 @@ const Login = () => {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" placeholder="Password" />
                         </Form.Group>
-                        <div id="errorMessage"></div>
+                        <div id="errorMessage" ></div>
                         <Button className="loginBtn" variant="primary" type="button" onClick={Login}>Login</Button>
                     </Form>
-                    <a className="loginLink" href = "/guest/sign-up">Create an Account</a>
+                    <a className="loginLink" href = "/guest/chooseSignup">Create an Account Here</a>
+                    <hr/>
+                    <GoogleOAuthProvider clientId={config.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+                        <GoogleLogin
+                        onSuccess={onSuccess}
+                        onFailure={onFailure}
+                        width="100%"
+                        />
+                    </GoogleOAuthProvider>
+                    
                 </div>
             </div>
         </div>
